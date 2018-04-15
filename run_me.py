6abb066b71d1abcdf7f71fffddd1825c6e841f79
J-Egeberg <jregeberg@gmail.com>
2018-04-15 12:03:34 +0200

    run and downloaded the csv

--- run_me.py
+++ run_me.py
@@ -12,19 +12,19 @@ def run():
     download_csv_sheet(csv_sheet_name, url)
     with open(csv_sheet_name, encoding='utf-8') as f:
         print_question_separator('1. What main-category of project has the highest success rate?')
-        question1.run(f)
+        
 
         print_question_separator('2. For the main-category of project with highest success rate (question above), what is the category with the highest number of project proposals?')
-        question2.run(f)
+        
 
         print_question_separator('3. What is the median pledged amount (usd_pledged_real) of successfully funded projects?')
-        question3.run(f)
+        
 
         print_question_separator('4. What is the number of successfully funded projects with more than 5.000$ pledged (usd_pledged_real) per category? (OPTIONAL: In case you decide to visualize all successful projects per category and you perceive any clusters in theresults, try to use the MeanShift algorithm on to see if your gut feeling was correct')
-        question4.run(f)
+        
 
         print_question_separator('5. For the main-category with the most successfully funded projects (quantity, not rate of success), what is the goal-amount range (usd_goal_real), e.g. range 0-10k$ , 5-15k$, 100k$-110k$, that contains the most successfully fundedprojects (in quantity, not rate of success)?')
-        question5.run(f)
+        
 
 
 def print_question_separator(question_number):
